e04e165e3b122badf8ee583a261bcb838256d498
Jason Huang <jason97521@gmail.com>
2023-04-22 11:24:42 -0400

    add item to mysql and file upload

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.js"
 import * as dotenv from 'dotenv'
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import multer from "multer"
 
 dotenv.config()
 const app = express()
@@ -13,11 +14,27 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null,Date.now()+file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/api/upload', upload.single('file'), function (req, res, next) {
+    const file = req.file
+    res.status(200).json(file.filename)
+})
+
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 
 
-app.listen(8800,()=>{
+app.listen(8800, () => {
     console.log("Connected!")
 })
