f5029d7a5b1a06273d02206bc8c50ac3257e9b10
Jason Huang <jason97521@gmail.com>
2023-04-22 12:06:44 -0400

    react mysql create and update post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -46,6 +46,11 @@ const Home = () => {
   //   },
   // ];
 
+  const getText=(html)=>{
+    const doc = new DOMParser().parseFromString(html,"text/html")
+    return doc.body.textContent
+  }
+
 
   return (
     <div className='home'>
@@ -53,13 +58,13 @@ const Home = () => {
         {posts.map(post => (
           <div className="post" key={post.id}>
             <div className="img">
-              <img src={post.img} alt="" />
+              <img src={`./upload/${post.img}`} alt="" />
             </div>
             <div className="content">
               <Link className='link' to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
-              <p>{post.desc}</p>
+              <p>{getText(post.desc)}</p>
               <button>Read More</button>
             </div>
           </div>
