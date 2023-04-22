561db5353b761fe9032de6a9447d7f9d9cddc555
Jason Huang <jason97521@gmail.com>
2023-04-22 05:43:02 -0400

    mysql auth register/login with clientside

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ import express from "express"
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
+import * as dotenv from 'dotenv'
+import cors from "cors"
 
+dotenv.config()
 const app = express()
-
 app.use(express.json())
+app.use(cors())
+
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
