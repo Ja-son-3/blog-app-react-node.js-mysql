df61adae45b5ea73364ba98a6867f0183b008125
Jason Huang <jason97521@gmail.com>
2023-04-22 05:52:02 -0400

    mysql and jwt login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import {db} from "../db.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const register = (req,res)=>{
     
@@ -32,7 +33,7 @@ export const login = (req,res)=>{
     //check user
     const q = "SELECT * FROM users WHERE username = ?"
 
-    db.query(q,[req,body,username], (err,data)=>{
+    db.query(q,[req.body.username], (err,data)=>{
         if (err) return res.json(err)
         if (data.length === 0) return res.status(404).json("User not found!")
 
@@ -41,7 +42,13 @@ export const login = (req,res)=>{
 
         if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
 
-        
+        const token = jwt.sign({id:data[0].id}, process.env.JWT_KEY)
+        const {password, ...other} = data[0]
+
+        res.cookie("access_token", token,{
+            httpOnly: true,
+        }).status(200).json(other)
+
     })
 }
 
--- api/index.js
+++ api/index.js
@@ -4,11 +4,14 @@ import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
 import * as dotenv from 'dotenv'
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 const app = express()
+
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
